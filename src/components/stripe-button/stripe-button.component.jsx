1ffdb08ba3d44f04dc63449c64dabe037c61f9df
adithyakam <50355901+adithyakam@users.noreply.github.com>
2020-09-01 09:01:26 +0530

    addinghooks

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -4,7 +4,7 @@ import StripeCheckout from "react-stripe-checkout";
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey =
-    "pk_test_51HGb9dCgtVEMMSlAv6nacRAG6UFhqOrcao58OU28XqcshPvwqbDDf3cm6Q1t3EcpZUHBmGxe8uQYBVIIkKt38Ry400954qD00W";
+    " pk_test_51HGb9dCgtVEMMSlAv6nacRAG6UFhqOrcao58OU28XqcshPvwqbDDf3cm6Q1t3EcpZUHBmGxe8uQYBVIIkKt38Ry400954qD00W";
 
   const onToken = (token) => {
     console.log(token);
